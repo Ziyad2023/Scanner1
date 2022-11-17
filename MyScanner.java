879320aac5a127cc27c6ff52a10ec84a5d759cb1
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-11-17 11:44:43 -0800

    almost done

---
+++ MyScanner.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            if (scanner.hasNext()) {
+                // 3
+                //String str = scanner.next();
+                //System.out.println(str);
+            }
+            
+            if (scanner.hasNextInt()) {
+                // 4
+                //int i = scanner.nextInt();
+                //System.out.println(i*3);
+            }
+            
+            if (scanner.hasNext()) {
+                // 5
+                String str = scanner.next();
+                System.out.println(str);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
+
